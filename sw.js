004b3362be51ba56daae81a5cfa2a0e8900a01b2
isaacoolaio-arch <isaac.oola.io@gmail.com>
2026-07-14 15:41:22 +0300

    Add in-app subtitle burn-in (ffmpeg.wasm single-thread), fix key wording (SW v5)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oola-translate-v4';
+const CACHE='oola-translate-v5';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
